df495cd177b2d50ce961345d00951e1ba20124b7
sobuj <sobuj.ah@gmail.com>
2015-10-22 00:23:04 +0600

    Config

--- src/FormiginationUI/Controllers/AccountController.cs
+++ src/FormiginationUI/Controllers/AccountController.cs
@@ -106,7 +106,7 @@ namespace FormiginationUI.Controllers
             EnsureDatabaseCreated(_applicationDbContext);
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.NickName, Email = model.Email };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
