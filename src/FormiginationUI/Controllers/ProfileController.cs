00e9f1b5ad318e550ceac67ccf26806c74412176
sobuj <sobuj.ah@gmail.com>
2015-10-23 10:38:04 +0600

    Contoller

---
+++ src/FormiginationUI/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Mvc;
+using FormiginationUI.Models;
+
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FormiginationUI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult ProfileSettings()
+        {
+            return View();
+        }
+
+        public IActionResult SaveProfile(ProfileSettingsViewModel model)
+        {
+           
+           
+
+            return Json("OK");
+        }
+    }
+}
