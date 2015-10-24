b6e7672bc0a9b6d7095d5d45f2fa8fbe67ccb721
sobuj <sobuj.ah@gmail.com>
2015-10-24 11:22:36 +0600

    Employment

---
+++ Ops.ViewModels/Ops/CompanyBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsModels.Ops
+{
+    public class CompanyBusiness
+    {
+        public int CompanyBusinessId { get; set; }
+        public string CompanyBusinessTitle { get; set; }
+
+
+    }
+}
---
+++ Ops.ViewModels/Ops/Employment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsModels.Ops
+{
+    public class Employment
+    {
+        public GeneralAccount GeneralAccount { get; set; }
+        public int EmploymentId { get; set; }
+        public string CompanyName { get; set; }
+
+        public CompanyBusiness CompanyBusiness { get; set; }
+
+        public string CompanyLocation { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Responsibilities { get; set; }
+        public DateTime EmploymentDateFrom { get; set; }
+        public DateTime EmploymentDateTo { get; set; }
+        public bool IsWorking { get; set; }
+
+    }
+}
